7ee45178e7e8398de93d9528d456db6323765c94
Jacek Generowicz <jacg@my-post-office.net>
2021-06-05 17:24:20 +0200

    Add check for LTO success in status code of main

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+#include <string>
 #include "rust_part.h"
 #include <chrono>
+#include <tuple>
 
 const std::string &get_name(const Person &person)
 {
@@ -42,31 +44,52 @@ int test_inline()
     return sum;
 }
 
-void test_lto()
+auto test_lto()
 {
     auto t1 = std::chrono::high_resolution_clock::now();
     auto sum = test_fun();
     auto t2 = std::chrono::high_resolution_clock::now();
 
-    auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(t2 - t1).count();
+    auto duration_r = std::chrono::duration_cast<std::chrono::nanoseconds>(t2 - t1).count();
 
     std::cout << "Calling rust function"
-              << ", time elapsed: " << duration << " ns." << std::endl;
+              << ", time elapsed: " << duration_r << " ns." << std::endl;
 
     t1 = std::chrono::high_resolution_clock::now();
     sum = test_inline();
     t2 = std::chrono::high_resolution_clock::now();
-    duration = std::chrono::duration_cast<std::chrono::nanoseconds>(t2 - t1).count();
+    auto duration_c = std::chrono::duration_cast<std::chrono::nanoseconds>(t2 - t1).count();
 
     std::cout << "Calling c++ function"
-              << ", time elapsed: " << duration << " ns." << std::endl;
+              << ", time elapsed: " << duration_c << " ns." << std::endl;
+
+    return std::make_tuple(duration_r, duration_c);
 }
 
-int main()
+int main(int argc, char** argv)
 {
+    if (argc != 2) {
+      std::cerr
+        << "One argument expected: ON | OFF, indicating whether LTO is enabled."
+        << std::endl;
+      return EXIT_FAILURE;
+    }
+    bool lto_on = argv[1] == std::string("ON");
+
     auto thing = rust_part::make_shared_thing();
     rust_part::print_shared_thing(thing);
 
-    test_lto();
-    return 0;
+    auto [d_rust, d_cpp] = test_lto();
+
+    bool rust_much_slower_than_cpp = d_rust / d_cpp > 10;
+
+    if (lto_on && rust_much_slower_than_cpp) {
+      std::cerr
+        << "Cross-language LTO appears not to be working as Rust is too slow:\n  "
+        << d_rust << " ns in Rust vs " << d_cpp << " ns in C++."
+        << std::endl;
+      return EXIT_FAILURE;
+    }
+    return EXIT_SUCCESS;
+
 }
